4fd7f5c67ded25a8175cee14aa8eb7ffa097c429
dalexandru89 <73753953+dalexandru89@users.noreply.github.com>
2023-01-21 19:05:13 +0100

    initial commit

--- Chapter01/src/components/List.js
+++ Chapter01/src/components/List.js
@@ -1,31 +1,49 @@
-import { useEffect, useState } from 'react';
-import Character from './Character';
+import React, { useState, useEffect, useMemo } from "react";
+import Character from "./Character";
+import SearchBox from "./SearchBox";
 
 function List() {
   const [characters, setCharacters] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [searchValue, setSearchValue] = useState("");
 
   useEffect(() => {
-    async function fetchData() {
-      const data = await fetch('https://rickandmortyapi.com/api/character');
-
-      const { results } = await data.json();
-
-      setCharacters(results);
-      setLoading(false);
+    async function fetchData(url) {
+      setLoading(true);
+      try {
+        while (url) {
+          const res = await fetch(url);
+          const data = await res.json();
+          setCharacters((prevCharacters) => [
+            ...prevCharacters,
+            ...data.results
+          ]);
+          url = data.info?.next;
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
     }
+    fetchData("https://rickandmortyapi.com/api/character");
+  }, []);
 
-    fetchData();
-  }, [characters.length]);
+  const filteredCharacters = useMemo(() => {
+    return characters.filter((character) =>
+      character.name.toLowerCase().includes(searchValue.toLowerCase())
+    );
+  }, [characters, searchValue]);
 
   return (
     <div>
+      <SearchBox onSearch={setSearchValue} />
       <h2>Characters</h2>
-      <div className='row'>
+      <div className="row">
         {loading ? (
           <div>Loading...</div>
         ) : (
-          characters.map((character) => (
+          filteredCharacters.map((character) => (
             <Character
               key={character.id}
               name={character.name}
--- Chapter01/src/index.js
+++ Chapter01/src/index.js
@@ -1,20 +1,19 @@
-import ReactDOM from 'react-dom/client';
-import List from './components/List';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import ReactDOM from "react-dom/client";
+import List from "./components/List";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className='container'>
-      <nav className='navbar sticky-top navbar-light bg-dark'>
-        <h1 className='navbar-brand text-light'>Rick and Morty</h1>
+    <div className="container">
+      <nav className="navbar sticky-top navbar-light bg-dark">
+        <h1 className="navbar-brand text-light">Rick and Morty</h1>
       </nav>
-
       <List />
     </div>
   );
 }
 
-const container = document.getElementById('app');
+const container = document.getElementById("app");
 
 const root = ReactDOM.createRoot(container);
 
